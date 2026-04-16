43797a4a20dca82a6a4839ad2dc0e254e4518c48
André Alencar <139161139+dedectr@users.noreply.github.com>
2026-04-16 20:30:33 -0300

    Update alpine-vps.sh

--- alpine-vps.sh
+++ alpine-vps.sh
@@ -1,30 +1,30 @@
 #!/bin/sh
 
-echo "[+] Instalando OpenVPN..."
+set -e
+
+echo "[+] Instalando pacotes..."
 apk update
-apk add openvpn easy-rsa iptables
+apk add openvpn easy-rsa iptables curl
+
+# Detectar interface automaticamente
+IFACE=$(ip route | grep default | awk '{print $5}')
+IP=$(curl -s ifconfig.me || hostname -I | awk '{print $1}')
+
+echo "[+] Interface detectada: $IFACE"
+echo "[+] IP detectado: $IP"
 
 echo "[+] Configurando Easy-RSA..."
 make-cadir /etc/openvpn/easy-rsa
-cd /etc/openvpn/easy-rsa || exit
+cd /etc/openvpn/easy-rsa
 
-# Inicializa PKI
 ./easyrsa init-pki
-
-echo "[+] Criando CA..."
 echo | ./easyrsa build-ca nopass
-
-echo "[+] Criando certificado do servidor..."
 ./easyrsa gen-req server nopass
 echo yes | ./easyrsa sign-req server server
-
-echo "[+] Gerando Diffie-Hellman..."
 ./easyrsa gen-dh
 
-echo "[+] Gerando TLS key..."
 openvpn --genkey --secret ta.key
 
-echo "[+] Copiando arquivos..."
 cp pki/ca.crt /etc/openvpn/
 cp pki/private/server.key /etc/openvpn/
 cp pki/issued/server.crt /etc/openvpn/
@@ -48,6 +48,7 @@ persist-key
 persist-tun
 
 push "redirect-gateway def1 bypass-dhcp"
+push "dhcp-option DNS 1.1.1.1"
 push "dhcp-option DNS 8.8.8.8"
 
 keepalive 10 120
@@ -59,15 +60,62 @@ status openvpn-status.log
 verb 3
 EOF
 
-echo "[+] Ativando IP Forward..."
-echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
-sysctl -p
+echo "[+] Ativando IP forward..."
+echo "net.ipv4.ip_forward=1" > /etc/sysctl.d/99-openvpn.conf
+sysctl -p /etc/sysctl.d/99-openvpn.conf
 
 echo "[+] Configurando NAT..."
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -o $IFACE -j MASQUERADE
+
+# Salvar regra NAT
+apk add iptables-save
+rc-service iptables save
+
+echo "[+] Criando cliente..."
+cd /etc/openvpn/easy-rsa
+./easyrsa gen-req client nopass
+echo yes | ./easyrsa sign-req client client
+
+CLIENT_OVPN=/root/client.ovpn
+
+cat > $CLIENT_OVPN <<EOF
+client
+dev tun
+proto udp
+remote $IP 1194
+resolv-retry infinite
+nobind
+persist-key
+persist-tun
+remote-cert-tls server
+cipher AES-256-CBC
+key-direction 1
+verb 3
+
+<ca>
+$(cat /etc/openvpn/ca.crt)
+</ca>
+
+<cert>
+$(cat pki/issued/client.crt)
+</cert>
+
+<key>
+$(cat pki/private/client.key)
+</key>
+
+<tls-auth>
+$(cat /etc/openvpn/ta.key)
+</tls-auth>
+EOF
 
 echo "[+] Iniciando OpenVPN..."
 rc-service openvpn start
 rc-update add openvpn
 
-echo "[+] Pronto! Agora falta criar cliente."
+echo ""
+echo "===================================="
+echo "[✔] VPN PRONTA!"
+echo "Arquivo do cliente:"
+echo "$CLIENT_OVPN"
+echo "===================================="
